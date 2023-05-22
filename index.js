7229c85bed177d32d4ca0fdcb30e257e2a6b58d5
Vaibhav D S <vaibhav.d.s@sap.com>
2023-05-22 19:24:23 +0530

    Basic Working

---
+++ index.js
@@ -0,0 +1,24 @@
+function login() {
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+  if (username.length > 0 && password.length > 0) {
+    if (username == "aa" && password == "a") {
+      window.location.replace("http://127.0.0.1:5500/shop.html");
+    } else {
+      alert("Wrong credentials");
+    }
+  } else {
+    alert("Ener username and password");
+  }
+  return false;
+}
+
+function showProduct() {
+  var ele = document.getElementById("search").value;
+  if (ele.length == 0) {
+    alert("Product name is empty");
+  } else if (ele.toLowerCase() == "box") {
+    let elem = document.getElementById("items");
+    elem.style.visibility = "visible";
+  }
+}
